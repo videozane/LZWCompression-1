0f35beb4d598cd6886f00aec3c6f06ef3fc14001
Aaron Cho <aars.cho10@gmail.com>
2021-09-21 00:21:54 -0700

    THE DECODER WORKS OMGOGMOGMOFKSGMNSDOM
    I finished this and it outputs an accurate file!!!

--- src/Decoder.java
+++ src/Decoder.java
@@ -2,58 +2,105 @@ import java.util.*;
 import java.io.*;
 
 public class Decoder {
-    
-/**
- * wtf is this 
- * 
- * PSEUDOCODE
-1    Initialize table with single character strings
-2    OLD = first input code
-3    output translation of OLD
-4    WHILE not end of input stream
-5        NEW = next input code
-6        IF NEW is not in the string table
-7               S = translation of OLD
-8               S = S + C
-9       ELSE
-10              S = translation of NEW
-11       output S
-12       C = first character of S
-13       OLD + C to the string table
-14       OLD = NEW
-15   END WHILE
- */
-
-
-    //IM SORRY CHRIS!!!!!
 
     private BufferedReader in;
     private int tableEntry = 256;
     private HashMap <Integer, String> dictionary;
+    private StringBuilder output;
 
 
+    /**
+     * This is the constructor :p
+     * @param filename
+     * @throws IOException
+     */
     public Decoder (String filename) throws IOException{
+    	
+    	
         //for file reading
         in = new BufferedReader (new FileReader(new File (filename)));
+        
+        dictionary = new HashMap <Integer, String>();
+        output = new StringBuilder ();
+        
+        output.append("");
+        
+        //inputting all of the dictionary values of each individual character.
         for (int i = 0; i < 256; i++) {
+        	
             dictionary.put(i, "" + (char) i);
+            
         }
 
     }
 
-    //decodes
-    public void Decode () throws IOException {
-
-        while (true) {
-
-            String cur = "" + in.read();
-            
-        }
+    
+    /**
+     * decodes the encoded file that contains encoded integer values for characters separated by ", " (a comma followed by a space).
+     * @throws IOException
+     */
+    public void decode () throws IOException {
+    	
+    	
+    	//Input of encoded numbers.
+    	String cur = in.readLine();
+    	
+    	//puts each encoded number as a String into an array of Strings called nums.
+    	//look I made this easy for you. Turn this array nums into an ArrayList to make it run a lot faster.
+    	String [] nums = cur.split(", ", 0);
+    	
+    	int old = Integer.parseInt(nums[0]);
+    	String s = dictionary.get(old);
+    	String c = "" + s.charAt(0);
+    	
+    	output.append(s);
+    	
+    	//loops through and does the actual algorithm. Followed the GeeksForGeeks pseudocode.
+    	for (int i = 1; i < nums.length; i++) {
+    		
+    		int next = Integer.parseInt(nums[i]);
+    		
+    		if (!dictionary.containsKey(next)) {
+    			s = dictionary.get(old);
+    			s = s+c;
+    		}
+    		
+    		else
+    			s = dictionary.get(next);
+    		
+    		output.append(s);
+    		c = "" + s.charAt(0);
+    		dictionary.put(tableEntry, dictionary.get(old) + c);
+    		tableEntry++;
+    		old = next;
+    		
+    	}
     }
 
     //outputs a file
-    public void fileOut () {
-
+    public void fileOut () throws Exception, IOException {
+    	FileWriter outputFile;
+		BufferedWriter writer = null;
+		if (output.equals("")) {
+			throw new Exception ("ayo you didn't decode anything cuh");
+		}
+		try{
+			outputFile = new FileWriter("DecodedOutput.txt");
+			writer = new BufferedWriter(outputFile);
+			writer.write(output.toString());
+			writer.close();
+		}
+		catch(Exception e)
+		{
+			
+		}
+    }
+    
+    public static void main (String [] args) throws Exception, IOException {
+    	Decoder dec = new Decoder ("EncodedOutput.txt");
+    	dec.decode();
+    	dec.fileOut();
+    	
     }
 
 
